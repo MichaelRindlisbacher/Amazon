acde37727df3ebc59241958f3b0eaf8242060914
MichaelRindlisbacher <micrindi2020@gmail.com>
2025-03-17 18:47:11 -0600

    Initial commit

---
+++ backend/Amazon.API/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amazon.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _context;
+        public BookController(BookDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            var something = _context.Books.ToList();
+            return something;
+        }
+    }
+}
